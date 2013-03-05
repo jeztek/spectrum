5eab58aee9ea282dca5bb67b8ff98068e4451ad6
Luke Alonso <lalonso@twitter.com>
2013-03-04 17:00:01 -0800

    Default modulation.

--- gnuradio/narrowband/benchmark_tx3.py
+++ gnuradio/narrowband/benchmark_tx3.py
@@ -134,7 +134,7 @@ def main():
     expert_grp = parser.add_option_group("Expert")
 
     parser.add_option("-m", "--modulation", type="choice", choices=mods.keys(),
-                      default='psk',
+                      default='qam',
                       help="Select modulation from: %s [default=%%default]"
                             % (', '.join(mods.keys()),))
 
